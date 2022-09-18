ffebbdf5a5f0cb22d3923a9c4bd8fd14ab9ff912
Chew <chew@chew.pw>
2022-09-17 20:20:20 -0500

    initial gradle

---
+++ src/main/java/pw/chew/cubedworldgen/CubedWorldGen.java
@@ -0,0 +1,17 @@
+package pw.chew.cubedworldgen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CubedWorldGen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
